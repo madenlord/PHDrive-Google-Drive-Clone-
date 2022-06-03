a6a59a0d3bffbfb784ce5d49f6afcd2e2edcfb68
Aaron Murillo <madenlordhobbies@gmail.com>
2022-06-03 12:05:44 +0200

    phdrive-storage.service.ts => implemented basic REST operations.

--- src/app/shared/services/phdrive-storage.service.ts
+++ src/app/shared/services/phdrive-storage.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { FileEntity } from '../models/fileEntity';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhdriveStorageService {
 
-  constructor() { }
+  private endpoint: string = "http://localhost:8080/PHDriveSTS/";
+
+  private operations = {
+    post: this.endpoint + "upload/",
+    get: this.endpoint + "download/",
+    delete: this.endpoint + "delete/"
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  uploadFile(file: File, filePath: string): Observable<FileEntity> {
+    filePath = filePath ? filePath : "/";
+
+    const formData = new FormData().append("file", file);
+
+    return this.httpClient.post<FileEntity>(this.operations.post, formData,
+           this.httpFileRequestParamBuilder(filePath));
+  }
+
+  downloadFile(filePath: string): Observable<File> {
+    return this.httpClient.get<File>(this.operations.get,
+           this.httpFileRequestParamBuilder(filePath));
+  }
+
+  deleteFile(filePath: string): Observable<any> {
+    return this.httpClient.delete<FileEntity>(this.operations.delete,
+           this.httpFileRequestParamBuilder(filePath));
+  }
+
+
+  private httpFileRequestParamBuilder(filePath: string) {
+    return filePath ? {params: new HttpParams().append("path", filePath)} : {};
+  }
 }
