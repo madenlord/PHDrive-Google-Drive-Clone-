6c63d0ca6a0a66110ed76cfb99c492ae093f907d
Aaron Murillo <madenlordhobbies@gmail.com>
2022-06-10 18:12:22 +0200

    Added icons and mocked data for first implementations

---
+++ src/app/shared/models/mocked-data.ts
@@ -0,0 +1,27 @@
+export const homeDir = {
+  path: "Home",
+  dirs: [
+    "Folder 1",
+    "Folder 2"
+  ],
+  files: [
+    "test.txt"
+  ]
+};
+
+export const folder1 = {
+  path: "Home/Folder 1",
+  dirs: [],
+  files: []
+}
+
+export const folder2 = {
+  path: "Home/Folder 2",
+  dirs: [],
+  files: []
+}
+
+export const testTxt = {
+  filename: "test.txt",
+  path: "Home"
+}
