3415cb537ab7f7c19170fd1a81af0b46fe29bf05
MadenLord <madenlordhobbies@gmail.com>
2022-06-11 15:10:08 +0200

    Deleted unnecesary code app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NGXLogger } from 'ngx-logger';
-import { FolderEntity } from './shared/models/folderEntity';
-import { homeDir } from './shared/models/mocked-data';
-import { PhdriveNavigationService } from './shared/services/phdrive-navigation.service';
-import { PhdriveStorageService } from './shared/services/phdrive-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +7,9 @@ import { PhdriveStorageService } from './shared/services/phdrive-storage.service
 })
 export class AppComponent implements OnInit {
 
-  data!: FolderEntity;
-  private homeFolder: FolderEntity = homeDir; // TODO: change for real services.
-
   constructor(
-    private logger: NGXLogger,
-    private folderService: PhdriveNavigationService,
-    private fileService: PhdriveStorageService,
   ) {}
 
   ngOnInit(): void {
-    this.folderService.getFolderContent(".").subscribe(folderContent =>
-      this.data = folderContent
-    );
   }
 }
