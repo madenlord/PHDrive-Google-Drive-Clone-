11ae0c0eafea1b3550cde662be6259e6d99e13a5
MadenLord <madenlordhobbies@gmail.com>
2022-06-16 12:43:50 +0200

    File phdrive-content code in another angular component

--- src/app/content/phdrive-content/phdrive-content-files/phdrive-content-files.component.ts
+++ src/app/content/phdrive-content/phdrive-content-files/phdrive-content-files.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NGXLogger } from 'ngx-logger';
+import { saveAs } from 'file-saver';
+import { FileEntity } from 'src/app/shared/models/fileEntity';
+import { PhdriveStorageService } from 'src/app/shared/services/phdrive-storage.service';
 
 @Component({
   selector: 'app-phdrive-content-files',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhdriveContentFilesComponent implements OnInit {
 
-  constructor() { }
+  @Input() files!: string[];
+  @Output() filesChange: EventEmitter<string[]> = new EventEmitter<string[]>();
+  @Input() folderPath!: string;
+
+  constructor(
+    private logger: NGXLogger,
+    private storageService: PhdriveStorageService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  downloadFile(fileName: string): void {
+    this.logger.log(this.folderPath + "/" + fileName);
+    this.storageService.downloadFile(this.folderPath + "/" + fileName).subscribe(response => {
+      this.logger.log(response);
+      saveAs(new File([response.body!], fileName,
+             {type: `${response.headers.get('Content-Type')};charset=utf-8`}));
+    }
+    );
+  }
+
+  uploadFile(event: any): void {
+    const file: File = event.target.files[0];
+    const formData = new FormData();
+    let fileInfo: FileEntity;
+
+    formData.append("file", file, file.name);
+    formData.append("path", this.folderPath + "/");
+    if(file) {
+      this.storageService.uploadFile(formData).subscribe((httpResponse: HttpResponse<Object>) => {
+        fileInfo = httpResponse.body as FileEntity;
+        this.files.push(fileInfo.filename);
+      }
+      );
+    }
   }
 
 }
--- src/app/content/phdrive-content/phdrive-content.component.ts
+++ src/app/content/phdrive-content/phdrive-content.component.ts
@@ -3,9 +3,6 @@ import { FolderEntity } from 'src/app/shared/models/folderEntity';
 import { NGXLogger } from 'ngx-logger';
 import { PhdriveNavigationService } from 'src/app/shared/services/phdrive-navigation.service';
 import { PhdriveStorageService } from 'src/app/shared/services/phdrive-storage.service';
-import { saveAs } from 'file-saver';
-import { HttpResponse } from '@angular/common/http';
-import { FileEntity } from 'src/app/shared/models/fileEntity';
 
 @Component({
   selector: 'app-phdrive-content',
@@ -31,31 +28,5 @@ export class PhdriveContentComponent implements OnInit {
     this.folderService.getFolderContent(this.folderPath).subscribe(folderContent =>
       this.data = folderContent
     );
-  }
-
-  downloadFile(fileName: string): void {
-    this.logger.log(this.folderPath + "/" + fileName);
-    this.fileService.downloadFile(this.folderPath + "/" + fileName).subscribe(response => {
-      this.logger.log(response);
-      saveAs(new File([response.body!], fileName,
-             {type: `${response.headers.get('Content-Type')};charset=utf-8`}));
-    }
-    );
-  }
-
-  uploadFile(event: any): void {
-    const file: File = event.target.files[0];
-    const formData = new FormData();
-    let fileInfo: FileEntity;
-
-    formData.append("file", file, file.name);
-    formData.append("path", this.folderPath + "/");
-    if(file) {
-      this.fileService.uploadFile(formData).subscribe((httpResponse: HttpResponse<Object>) => {
-        fileInfo = httpResponse.body as FileEntity;
-        this.data.files.push(fileInfo.filename);
-      }
-      );
-    }
   }
 }
