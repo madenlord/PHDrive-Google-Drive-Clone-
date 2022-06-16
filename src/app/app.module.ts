6819f27b8c01a51d71cfa4e72663e9280f37597f
MadenLord <madenlordhobbies@gmail.com>
2022-06-16 13:37:35 +0200

    Added package "ngx-toastr" to use toasters notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 import { PhdriveNavbarComponent } from './navbar/phdrive-navbar/phdrive-navbar.component';
 import { PhdriveContentComponent } from './content/phdrive-content/phdrive-content.component';
 import { PhdriveContentFoldersComponent } from './content/phdrive-content/phdrive-content-folders/phdrive-content-folders.component';
 import { PhdriveContentFilesComponent } from './content/phdrive-content/phdrive-content-files/phdrive-content-files.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { PhdriveContentFilesComponent } from './content/phdrive-content/phdrive-
     HttpClientModule,
     LoggerModule.forRoot({
       level: NgxLoggerLevel.DEBUG
-    })
+    }),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
